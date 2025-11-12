58c2556a0c3d7a671017b07cf8498f5c0644419f
ist14k <ist14k.akash@gmail.com>
2025-11-12 22:53:45 +0600

    feat: add TRPC integration with Prisma and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import prisma from "@/lib/prisma";
+import { caller } from "@/trpc/server";
 
-const page = () => {
+const page = async () => {
+  const user = await caller.getUsers();
   return (
     <div>
       <h1 className="text-4xl">Hello, World</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
 };

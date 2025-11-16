1115ad6316f5ceed26f3c4384130316cc259a7c5
ist14k <ist14k.akash@gmail.com>
2025-11-16 22:04:51 +0600

    feat: integrate Sentry for error tracking and monitoring.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,4 +1,5 @@
 import { inngest } from "./client";
+import * as Sentry from "@sentry/nextjs";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { createOpenAI } from "@ai-sdk/openai";
 import { createAnthropic } from "@ai-sdk/anthropic";
@@ -12,6 +13,9 @@ export const executeAi = inngest.createFunction(
   { id: "execute-ai" },
   { event: "test/execute.ai" },
   async ({ event, step }) => {
+    Sentry.logger.info("User triggered test log", {
+      log_source: "sentry_test",
+    });
     const { steps: geminiSteps } = await step.ai.wrap(
       "Generate text using Google Gemini",
       generateText,
@@ -19,27 +23,41 @@ export const executeAi = inngest.createFunction(
         model: googleAI("gemini-2.5-flash"),
         system: "You are a helpful assistant that generates creative text.",
         prompt: "Write a short poem about the sea.",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
-    const { steps: openaiSteps } = await step.ai.wrap(
-      "Generate text using OpenAI GPT-4",
-      generateText,
-      {
-        model: openai("gpt-4"),
-        system: "You are a helpful assistant that generates creative text.",
-        prompt: "Write a short poem about the sea.",
-      }
-    );
-    const { steps: anthropicSteps } = await step.ai.wrap(
-      "Generate text using Anthropic Claude",
-      generateText,
-      {
-        model: anthropic("claude-2"),
-        system: "You are a helpful assistant that generates creative text.",
-        prompt: "Write a short poem about the sea.",
-      }
-    );
-
-    return { geminiSteps, openaiSteps, anthropicSteps };
+    // const { steps: openaiSteps } = await step.ai.wrap(
+    //   "Generate text using OpenAI GPT-4",
+    //   generateText,
+    //   {
+    //     model: openai("gpt-4"),
+    //     system: "You are a helpful assistant that generates creative text.",
+    //     prompt: "Write a short poem about the sea.",
+    //     experimental_telemetry: {
+    //       isEnabled: true,
+    //       recordInputs: true,
+    //       recordOutputs: true,
+    //     },
+    //   }
+    // );
+    // const { steps: anthropicSteps } = await step.ai.wrap(
+    //   "Generate text using Anthropic Claude",
+    //   generateText,
+    //   {
+    //     model: anthropic("claude-2"),
+    //     system: "You are a helpful assistant that generates creative text.",
+    //     prompt: "Write a short poem about the sea.",
+    //     experimental_telemetry: {
+    //       isEnabled: true,
+    //       recordInputs: true,
+    //       recordOutputs: true,
+    //     },
+    //   }
+    // );
+    return geminiSteps;
   }
 );
